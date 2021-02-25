9cfbb72af32afc5b7a7b4bf60bf8114b1e28158e
arynoviyanto <arynoviyanto@gmail.com>
2021-02-26 07:54:27 +1300

    Tidy up, add intersection area for tracking object, to be continued ...

--- main.py
+++ main.py
@@ -1,105 +1,169 @@
 import cv2.cv2 as cv2
-import re
 import os
 import numpy as np
-import matplotlib.pyplot as plt
-import math
+import argparse
 
 from utils import assign_cars
 
-cap = cv2.VideoCapture("footage/sherbrooke_video.avi")
-# cap = cv2.VideoCapture("footage/highway.mp4")
 
-if not cap.isOpened():
-    print("Cannot read video")
-    exit()
+def analyse_footage(video_filename, roi_option, sample_background_filename=None,
+                    footage_dir="footage", frame_dir="frames"):
+    cap = cv2.VideoCapture(os.path.join(footage_dir, video_filename))
+    # cap = cv2.VideoCapture("footage/highway.mp4")
 
-backSub = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=150, detectShadows=True)
-# backSub = cv2.createBackgroundSubtractorKNN()
+    if sample_background_filename is not None:
+        background = cv2.imread(os.path.join(frame_dir, sample_background_filename))
+    else:
+        background = None
 
-kernel = np.ones((2, 2), np.uint8)
-cars = []
+    if not cap.isOpened():
+        print("Cannot read video")
+        exit()
 
-prevXc = 0
-prevYc = 0
-tick = 0
+    backSub = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=150, detectShadows=True)
 
-while True:
-    ret, frame = cap.read()
-    tick = tick + 1
+    kernel = np.ones((2, 2), np.uint8)
+    cars = []
 
-    if not ret:
-        print("Can't receive frame")
-        break
+    # prevXc = 0
+    # prevYc = 0
+    tick = 0
 
-    # plt.imshow(frame)
-    # plt.show()
+    delay = 10
 
-    fgMask = backSub.apply(frame)
+    while True:
+        ret, frame = cap.read()
+        tick = tick + 1
 
-    # _, fgMask = cv2.threshold(fgMask, 254, 255, cv2.THRESH_BINARY)
-    #
-    # fgMask = cv2.morphologyEx(fgMask, cv2.MORPH_OPEN, kernel)
-    # fgMask = cv2.morphologyEx(fgMask, cv2.MORPH_DILATE, kernel, iterations=3)
-    fgMask = cv2.morphologyEx(fgMask, cv2.MORPH_ERODE, kernel, iterations=1)
+        if not ret:
+            print("Can't receive frame")
+            break
 
-    contours, hierarchy = cv2.findContours(fgMask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        if background is None:
+            fg_mask = backSub.apply(frame)
+            # _, fg_mask = cv2.threshold(fg_mask, 254, 255, cv2.THRESH_BINARY)
+        else:
+            fg_mask = cv2.absdiff(background, frame)
+            fg_mask = cv2.cvtColor(fg_mask, cv2.COLOR_BGR2GRAY)
+            _, fg_mask = cv2.threshold(fg_mask, 15, 255, cv2.THRESH_BINARY)
 
-    y1 = 300
-    y2 = 486
-    x1 = 192
-    x2 = 784
-    minArea = 5000
+        fg_mask = cv2.morphologyEx(fg_mask, cv2.MORPH_OPEN, kernel, iterations=1)
 
-    current_cars = []
-    for contour in contours:
-        [x, y, w, h] = cv2.boundingRect(contour)
-        xc = int(x + w / 2)
-        yc = int(y + h / 2)
-        if (w * h > minArea) and (xc >= x1) and (xc <= x2) and (yc >= y1) and (yc <= y2):
-            deltaX = xc - prevXc + np.finfo(float).eps
-            deltaY = yc - prevYc + np.finfo(float).eps
+        contours, hierarchy = cv2.findContours(fg_mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
-            gradient = 1.0 * deltaY / deltaX
+        y1 = roi_option['y1']
+        y2 = roi_option['y2']
+        x1 = roi_option['x1']
+        x2 = roi_option['x2']
+        minArea = roi_option['minArea']
 
-            movement = deltaX * deltaX + deltaY * deltaY
-            # print("{0}, {1}: tick: {4}, grad = {2:.2f}, move = {3}".format(xc, yc, gradient, movement, tick))
-            prevYc = yc
-            prevXc = xc
-            car = {
-                'id': -1,
-                'center': (xc, yc),
-                'rect': (x, y, w, h),
-                'tick': tick,
-                'active': False
-            }
+        valid_contours = contours
 
-            current_cars.append(car)
+        # valid_contours = []
+        # for i, contour in enumerate(contours):
+        #     [cx1, cy1, cw1, ch1] = cv2.boundingRect(contour)
+        #     is_valid = True
+        #     for j, friend in enumerate(contours):
+        #         if i != j:
+        #             [cx2, cy2, cw2, ch2] = cv2.boundingRect(friend)
+        #             if (cx1 > cx2 and cy1 > cy2 and
+        #                     (cx1 + cw1) < (cx2 + cw2) and (cy1 + ch1) < (cy2 + ch2)):
+        #                 is_valid = False
+        #                 break
+        #
+        #     if is_valid:
+        #         valid_contours.append(contour)
 
-    assign_cars(cars, current_cars)
+        current_cars = []
+        for contour in valid_contours:
+            [x, y, w, h] = cv2.boundingRect(contour)
+            xc = int(x + w / 2)
+            yc = int(y + h / 2)
+            if (w * h > minArea) and (xc >= x1) and (xc <= x2) and (yc >= y1) and (yc <= y2):
+                # deltaX = xc - prevXc + np.finfo(float).eps
+                # deltaY = yc - prevYc + np.finfo(float).eps
 
-    for car in cars:
-        if car['active']:
-            [x, y, w, h] = car['rect']
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 1)
+                # gradient = 1.0 * deltaY / deltaX
+                # movement = deltaX * deltaX + deltaY * deltaY
+                # print("{0}, {1}: tick: {4}, grad = {2:.2f}, move = {3}".format(xc, yc, gradient, movement, tick))
 
-            cv2.putText(frame, str(car['id']), car['center'], cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 1, cv2.LINE_AA)
+                # prevYc = yc
+                # prevXc = xc
 
-            # cv2.drawContours(frame, [contour], 0, (0, 255, 0), 1)
-            # cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 255), 1)
+                car = {
+                    'id': -1,
+                    'center': (xc, yc),
+                    'rect': (x, y, w, h),
+                    'tick': tick,
+                    'active': False
+                }
 
-    cv2.putText(frame, "Number of cars: {0}".format(len(cars)), (100, 100), cv2.FONT_HERSHEY_SIMPLEX,
-                2, (0, 255, 0), 2, cv2.LINE_AA)
-    cv2.imshow('FG Mask', fgMask)
-    cv2.imshow("frame", frame)
+                current_cars.append(car)
 
-    key = cv2.waitKey(10)
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 1)
 
-    if key == 27:  # Escape
-        break
+            cv2.drawContours(frame, [contour], 0, (0, 255, 0), 1)
 
-    if key == 32:  # Space bar
-        cv2.waitKey(-1)  # wait until any key is pressed
+        assign_cars(cars, current_cars)
 
-cap.release()
-cv2.destroyAllWindows()
+        for car in cars:
+            if car['active']:
+                [x, y, w, h] = car['rect']
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 1)
+
+                cv2.putText(frame, str(car['id']), car['center'],
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 1, cv2.LINE_AA)
+
+                # cv2.drawContours(frame, [contour], 0, (0, 255, 0), 1)
+                # cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 255), 1)
+
+        cv2.putText(frame, "Number of cars: {0}".format(len(cars)), (100, 100), cv2.FONT_HERSHEY_SIMPLEX,
+                    2, (0, 255, 0), 2, cv2.LINE_AA)
+        cv2.imshow('FG Mask', fg_mask)
+        cv2.imshow("frame", frame)
+
+        key = cv2.waitKey(delay)
+
+        if key == 27:  # Escape
+            break
+
+        if key == 32:  # Space bar
+            cv2.waitKey(-1)  # wait until any key is pressed
+
+        if key == ord('s'):
+            delay = 10 * delay
+
+        if key == ord('f'):
+            delay = int(delay / 10)
+            delay = 1 if delay < 1 else delay
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--v', required=True,
+                    help="Video filename")
+parser.add_argument('--v_dir', default='footage',
+                    help="Footage directory")
+parser.add_argument('--b', default=None,
+                    help="Predefined background")
+parser.add_argument('--i_dir', default='frames',
+                    help="Frame directory")
+
+# python3 main.py --v sherbrooke_video.avi --b sherbrooke_frames/00000222.jpg
+if __name__ == "__main__":
+    # video_filename = "sherbrooke_video.avi"
+    # sample_background_filename = "sherbrooke_frames/00000222.jpg"
+
+    args = parser.parse_args()
+
+    roi = {
+        'y1': 300,
+        'y2': 486,
+        'x1': 192,
+        'x2': 784,
+        'minArea': 6000
+    }
+
+    analyse_footage(args.v, roi, args.b, footage_dir=args.v_dir, frame_dir=args.i_dir)
--- utils.py
+++ utils.py
@@ -1,6 +1,29 @@
 import numpy as np
 
-def assign_cars(cars, current_cars):
+
+def get_intersection_area(rect1, rect2):
+    r1_x1 = rect1[0]
+    r1_x2 = rect1[0] + rect1[2]
+
+    r2_x1 = rect2[0]
+    r2_x2 = rect2[0] + rect2[2]
+
+    w = np.min([r1_x2, r2_x2]) - np.max([r1_x1, r2_x1])
+    w = 0 if w < 0 else w
+
+    r1_y1 = rect1[1]
+    r1_y2 = rect1[1] + rect1[3]
+
+    r2_y1 = rect2[1]
+    r2_y2 = rect2[1] + rect2[3]
+
+    h = np.min([r1_y2, r2_y2]) - np.max([r1_y1, r2_y1])
+    h = 0 if h < 0 else h
+
+    return w * h
+
+
+def assign_cars(cars, current_cars, debug=False):
     delta_grad = 0.1
     delta_movement = 800
     delta_tick = 15
@@ -16,12 +39,27 @@ def assign_cars(cars, current_cars):
                 delta_x = candidate['center'][0] - car['center'][0] + np.finfo(float).eps
                 delta_y = candidate['center'][1] - car['center'][1] + np.finfo(float).eps
 
-                gradient = 1.0 * delta_y / delta_x
-                movement = delta_x * delta_x + delta_y * delta_y
+                # gradient = 1.0 * delta_y / delta_x
+                # movement = delta_x * delta_x + delta_y * delta_y
                 tick_grad = candidate['tick'] - car['tick']
 
-                print("tick_grad: {0}, gradient = {1:.2f}, movement = {2}".format(tick_grad, gradient, movement))
-                if tick_grad <= delta_tick and movement <= delta_movement * tick_grad:
+                # print("tick_grad: {0}, gradient = {1:.2f}, movement = {2}".format(tick_grad, gradient, movement))
+                # if tick_grad <= delta_tick and movement <= delta_movement * tick_grad:
+
+                inter_area = get_intersection_area(candidate['rect'], car['rect'])
+
+                if inter_area > 0.45 * car['rect'][2] * car['rect'][3] and tick_grad <= delta_tick:
+                    # print(inter_area)
+                    if debug:
+                        prev_car = {
+                            'id': car['id'],
+                            'center': car['center'],
+                            'rect': car['rect'],
+                            'tick': car['tick'],
+                            'active': True
+                        }
+                        cars.append(prev_car)
+
                     car['center'] = candidate['center']
                     car['rect'] = candidate['rect']
                     car['tick'] = candidate['tick']
